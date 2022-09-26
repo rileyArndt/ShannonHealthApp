85d6e1ca641289fdaaf4d27f3d4a6273248fb0bd
ndiazdelopediaz <ndiazdelopediaz@angelo.edu>
2022-09-26 11:40:14 -0500

    added email checking

--- LoginHashAlpha 1.py
+++ LoginHashAlpha 1.py
@@ -2,9 +2,12 @@ import hashlib as hash
 from datetime import date
 
 
-username = input("Username>:")
-sal = str(date.today()) # keep this to generate a salt and pass as encrypted data (not final)
-secret = hash.md5((sal + input("Password >:")).encode())
+username = input("Email Address>:") #accepts users email
+while '@' not in username or '.' not in username:
+    print("Please enter valid email")
+    username = input("Email Address>:") #accepts users email upon failure
+sal = str(date.today()) # keep this to generate a salt and pass as encrypted data
+secret = hash.md5((sal + input("Password >:")).encode())   #placeholder
 
 print(username + '\n' + sal + '\n' + secret.hexdigest())
 
