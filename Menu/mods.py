73bd31d156ce9602a973cd1d69caae7557848f94
mmartin46 <100044690+mmartin46@users.noreply.github.com>
2022-10-11 11:32:43 -0500

    mods.py
    Some of these modules can be useful for development in kivy.

---
+++ Menu/mods.py
@@ -0,0 +1,38 @@
+# Mitchell Martin
+# 10/11/2022
+#
+# Here are some useful modules for
+# experimenting with the kivy interface.
+#
+from kivy.core.window import Window
+from kivy.uix.label import Label
+from kivymd.uix.datatables import MDDataTable
+from kivymd.app import MDApp
+from kivymd.uix.button import MDFlatButton
+from kivy.lang.builder import Builder
+from kivy.lang import Builder
+from kivy.uix.screenmanager import Screen, ScreenManager
+from kivy_garden.mapview import MapView
+from kivymd.uix.list import MDList, TwoLineListItem, TwoLineAvatarListItem, OneLineListItem
+from kivy.uix.scrollview import ScrollView
+from kivy.metrics import dp
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivymd.uix.button import MDRectangleFlatButton
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
+import mysql.connector
+from numpy import minimum
+from kivy.uix.relativelayout import RelativeLayout
+from kivymd.uix.floatlayout import MDFloatLayout
+from kivy.graphics import Color, Canvas, Line, RoundedRectangle
+from kivymd.uix.button import MDIconButton
+from kivymd.uix.label import MDLabel
+from threading import Event
+from kivy.properties import StringProperty, NumericProperty
+from kivy.core.text import LabelBase
+from kivy.clock import Clock
+import webbrowser
+import time
