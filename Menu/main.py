c4d02be69f7325b55bed26e1aefaf673145f1ccc
mmartin46 <100044690+mmartin46@users.noreply.github.com>
2022-11-28 13:29:04 -0600

    Update main.py

--- Menu/main.py
+++ Menu/main.py
@@ -1,79 +1,189 @@
-"""
-User Authentification Functionality
-"""
+# Mitchell Martin
+# 10/11/2022
 
+# Adding all the modules
+from mods import *
 from str_builder import *
+from Perscription import perscreen
+from Chatbot import chatresponses, chatai, dataextraction
+from LoginInfo import *
 
-complete_app_builder += """
-<Login>:
-   name: 'login'
-   Screen:
-      MDBoxLayout:
-         pos_hint: {"center_x": 0.5, "center_y": 0.5}
-         padding: 25
-         spacing: 25
-         orientation: 'vertical'
-         
-         Image:
-            source: 'Icons\ShannonLogo.png'
-            pos_hint: {"center_x": 0.5}
-
-         MDLabel:
-            id: welcome_label
-            text: "Login"
-            font_size: 22
-            halign:'center'
-            size_hint_y: None
-            height: self.texture_size[1]
-            padding_y: 15
-
-         MDTextField:
-            id: user
-            hint_text: "Email"
-            icon_right: "account"
-            size_hint_x: None
-            width: 200
-            font_size: 18
-            pos_hint: {"center_x": 0.5}
+from kivymd.icon_definitions import md_icons
+from kivymd import icon_definitions
+import kivymd.uix.navigationdrawer
+from mysql.connector.locales.eng import client_error
+import mysql
+import kivymd.uix.datatables
+import kivymd.uix.dropdownitem
+from mysql.connector import Error
+import geocoder
+import socket
+import datetime
+import hashlib as hash
+import mods
 
 
-         MDTextField:
-            id: password
-            hint_text: "Password"
-            icon_right: "eye-off"
-            size_hint_x: None
-            width: 200
-            font_size: 18
-            pos_hint: {"center_x": 0.5}
-            password: True
 
-         MDLabel:
-            id: warning_label
-            text: " "
-            font_size: 12
-            halign:'center'
-            size_hint_y: None
-            height: self.texture_size[1]
+dbpass= os.environ.get('dbpass')
+mailpass= os.environ.get('mailpass')
 
-         MDRoundFlatButton:
-            text: "Log In"
-            font_size: 20
-            pos_hint: {"center_x": 0.5}
-            on_press: app.logger()
+class Login (Screen):
+    pass
+# The MainScreen class's functionality is on kivy. 
+#
+# Let me know if you
+# only want to make a screen using python.
+class MainScreen(Screen):
+   pass
 
-         MDFlatButton:
-            text: "Create account"
-            font_size: 12
-            pos_hint: {"center_x": 0.5}
-            on_release: root.manager.current = "create"
+class Create (Screen):
+    pass
 
-         MDFlatButton:
-            text: "Forgot Password"
-            font_size: 10
-            pos_hint: {"center_x": 0.5}
-            on_press: root.manager.current = "forgot"
+class Create2(Screen):
+    pass
 
-"""
+class Forgot (Screen):
+    pass
+
+class Forgot2 (Screen):
+    pass
+
+class Forgot3 (Screen):
+    pass
+
+class Physical_Screen(Screen):
+   pass
+
+   
+mods.username = 'default'
+
+def logger(self):
+   
+   connection = mysql.connector.connect(host = "shannontestdatabase.cxc8luynmyvm.us-east-1.rds.amazonaws.com",
+   user = "admin",
+   passwd =  dbpass)
+   print("Connection initialized")
+   cursor = connection.cursor()
+   cursor.execute("use shannon;")
+   
+   
+   def grabQuery(query):
+       connection = None 
+       try:
+           cursor.execute(query)
+           ret = cursor.fetchall()
+           print("Query successful")
+       except Error as err:
+           print(f"Error: '{err}'")
+       return ret
+
+   def insertQuery(query):
+       try:
+           cursor.execute(query)
+           connection.commit()
+           print("Query successful")
+       except Error as err:
+           print(f"Error: '{err}'")
+
+   mods.username = str(self.root.get_screen("login").ids.user.text)
+   
+   while '@' not in mods.username or '.' not in mods.username:
+       self.root.get_screen("login").ids.warning_label.text = "please enter a valid email"
+       return
+   secret = str(self.root.get_screen("login").ids.password.text)
+   sal = grabQuery(""" 
+   select sal from auth 
+   where email = '""" + mods.username +"';")
+   if len(sal) > 0:
+       sal = sal[0][0]
+       secret = hash.md5((sal + secret).encode())   #placeholder
+       secret = secret.hexdigest()
+       if secret == grabQuery("select phash from auth where email='" + mods.username + "';")[0][0]: 
+           city = geocoder.ip("me").city
+           realip = socket.gethostbyname(socket.gethostname())
+           time = str(datetime.datetime.now())[:-7]
+           insertQuery("update auth set location = '" + city + "', ip = '" + realip+"', logintime = '" + time + "' where email = '"+ mods.username+ "';")
+           insertQuery("insert into cart(usr_id) values ('" + mods.username + "');")
+           self.root.current = 'main'
+           print(mods.username)
+           return 
+       else:
+           self.root.get_screen("login").ids.warning_label.text = "Could not authenticate user, please re-enter your credentials."
+           return 
+   else:
+       self.root.get_screen("login").ids.warning_label.text = "Could not authenticate user, please re-enter your credentials."
+       return 
+
+
+   
+
+# Add all your created screens
+# to the screen manager.
+global sm
+sm = ScreenManager()
+sm.add_widget(Login(name='login'))
+sm.add_widget(MainScreen(name='main'))
+
+
+# Builds the application.
+class MainApp(MDApp):
+   def build(self):
+      screen = Builder.load_string(complete_app_builder)
+      return screen  
+   
+   def switch_mode(self):
+      self.theme_cls.theme_style = (
+         "Dark" if self.theme_cls.theme_style == "Light" else "Light"
+      )
+      pass
+   
+   def get_out(self):
+      self.root.current = 'login'
+      self.root.direction = 'left'  
+    
+
+MainApp.logger = logger
+MainApp.create = create
+MainApp.create2 = create2
+MainApp.forgot = forgot
+MainApp.forgot2 = forgot2
+MainApp.forgot3 = forgot3
+
+def user_name():
+  return sm.get_screen("login").ids.user.text
+
+
+class CustomOneLineIconListItem(OneLineIconListItem):
+   icon = StringProperty()
+   
+
+   def __init__(self, *args, **kwargs):
+      super().__init__(*args, **kwargs)
+      
+   def print_item(self, instance):
+      self.cartdb = mysql.connector.connect(
+         host = "shannontestdatabase.cxc8luynmyvm.us-east-1.rds.amazonaws.com",
+         user = "admin",
+         passwd = dbpass,
+         database = "shannon"
+      )
+      
+      self.c2 = self.cartdb.cursor()
+      query = "INSERT INTO cart SELECT id, product_name, price, '" + mods.username + "' FROM products WHERE id = '" + instance.text[:6] + "'"
+      print(query)
+      self.c2.execute(query)
+      
+      self.c2.execute(query)
+      self.cartdb.commit()
+
+
+    
+
+# Runs the application.
+if __name__ == '__main__':
+   MainApp().run()
+
+
 
 
 
